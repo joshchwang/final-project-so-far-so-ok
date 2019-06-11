c3a8cd6b2f5aee01200fbe5cc580ec6e7e3bf633
josh <joshua.chuang21@ycdsbk12.ca>
2019-06-10 21:32:41 -0400

    wooooo dotted line

--- Pong.py
+++ Pong.py
@@ -18,6 +18,9 @@ screens_list = [start_screen, instructions_screen,
                 game_1_player_ai_screen, game_2_player_screen,
                 game_survival, game_endless, game_over_screen]
 
+# States Booleans
+start_screen_ = True
+
 # Player 1 Variables
 player_1_x = 50
 player_1_y = 250
@@ -29,6 +32,13 @@ player_2_x = 950
 player_2_y = 250
 player_2_up_pressed = False
 player_2_down_pressed = False
+
+# Ai Variables
+is_ai = True
+ai_x = 950
+ai_y = 250
+ai_up = False
+ai_down = False
 
 # Ball Variables
 ball_x = 500
@@ -57,6 +67,7 @@ ball_bounce_up = True
 # Score
 score_player_1 = 9
 score_player_2 = 9
+score_ai = 9
 score_endless = 0
 score_lives_player_1 = 0
 score_lives_player_2 = 0
@@ -78,6 +89,10 @@ def on_update(delta_time):
 
     global score_player_1, score_player_2, score_lives_player_1, score_lives_player_2, score_endless
 
+    global is_ai, ai_x, ai_y
+
+    global start_screen
+
     # Player 1 Movement
     if player_1_up_pressed and player_1_y + 50 <= 490:
         player_1_y += 5
@@ -85,10 +100,15 @@ def on_update(delta_time):
         player_1_y -= 5
 
     # Player 2 Movement
-    if player_2_up_pressed and player_2_y + 50 <= 490:
+    if player_2_up_pressed and player_2_y + 50 <= 490 and not is_ai:
         player_2_y += 5
-    if player_2_down_pressed and player_2_y - 50 >= 0:
+    if player_2_down_pressed and player_2_y - 50 >= 0 and not is_ai:
         player_2_y -= 5
+
+    if ai_y + 50 <= 490 and is_ai:
+        pass
+    if ai_y - 59 >= 0 and is_ai:
+        pass
 
     # Ball Bouncing off of the Divider and the Bottom
     if ball_bounce_up:
@@ -104,7 +124,7 @@ def on_update(delta_time):
 
     # Increasing Ball Velocity
     if ball_velocity_check:
-        ball_velocity_check += 5
+        ball_velocity_check += 1
         ball_velocity_check = False
 
     # Check if the Ball Bounces Off Screen Player 1
@@ -135,13 +155,23 @@ def on_update(delta_time):
 def on_draw():
     global rectangle_list
 
+    global ball_y
+
+    global is_ai, ai_x, ai_y
+
     arcade.start_render()
 
     # Player 1
     arcade.draw_rectangle_outline(player_1_x, player_1_y, 10, 100, arcade.color.WHITE)
 
-    # Player 2
-    arcade.draw_rectangle_outline(player_2_x, player_2_y, 10, 100, arcade.color.WHITE)
+    if not is_ai:
+        # Player 2
+        arcade.draw_rectangle_outline(player_2_x, player_2_y, 10, 100, arcade.color.WHITE)
+
+    if is_ai:
+        # AI
+        arcade.draw_rectangle_outline(ai_x, ball_y, 10, 100, arcade.color.RED)
+        ai_y = ball_y
 
     # Ball
     arcade.draw_rectangle_outline(ball_x, ball_y, 10, 10, arcade.color.WHITE)
@@ -156,8 +186,13 @@ def on_draw():
     # Player 1 Text -------------------------- Replace string condition with variable for ability to choose name
     arcade.draw_text("Player 1", 10, 580, arcade.color.WHITE, 12)
 
-    # Player 2 Text -------------------------- Replace string condition with variable for ability to choose name
-    arcade.draw_text("Player 2", 940, 580, arcade.color.WHITE, 12)
+    if not is_ai:
+        # Player 2 Text -------------------------- Replace string condition with variable for ability to choose name
+        arcade.draw_text("Player 2", 940, 580, arcade.color.WHITE, 12)
+
+    if is_ai:
+        # AI Text
+        arcade.draw_text("AI", 980, 580, arcade.color.RED, 12)
 
     # Player 1 Score
     if score_player_1 < 10:
@@ -166,8 +201,13 @@ def on_draw():
     if score_player_1 >= 10:
         arcade.draw_text(str(score_player_1), 380, 525, arcade.color.WHITE, 70)
 
-    # Player 2 Score
-    arcade.draw_text(str(score_player_2), 520, 525, arcade.color.WHITE, 70)
+    if not is_ai:
+        # Player 2 Score
+        arcade.draw_text(str(score_player_2), 520, 525, arcade.color.WHITE, 70)
+
+    if is_ai:
+        # AI
+        arcade.draw_text(str(score_ai), 520, 525, arcade.color.RED, 70)
 
     # Colon
     arcade.draw_rectangle_filled(500, 570, 10, 10, arcade.color.WHITE)
@@ -179,6 +219,8 @@ def on_key_press(key, modifiers):
 
     global player_2_up_pressed, player_2_down_pressed
 
+    global is_ai
+
     # Player 1 Key Input
     if key == arcade.key.W:
         player_1_up_pressed = True
@@ -186,9 +228,9 @@ def on_key_press(key, modifiers):
         player_1_down_pressed = True
 
     # Player 2 Key Input
-    if key == arcade.key.UP:
+    if key == arcade.key.UP and not is_ai:
         player_2_up_pressed = True
-    if key == arcade.key.DOWN:
+    if key == arcade.key.DOWN and not is_ai:
         player_2_down_pressed = True
 
 
@@ -197,6 +239,8 @@ def on_key_release(key, modifiers):
 
     global player_2_up_pressed, player_2_down_pressed
 
+    global is_ai
+
     # Player 1 Key Output
     if key == arcade.key.W:
         player_1_up_pressed = False
@@ -204,9 +248,9 @@ def on_key_release(key, modifiers):
         player_1_down_pressed = False
 
     # Player 2 Key Output
-    if key == arcade.key.UP:
+    if key == arcade.key.UP and not is_ai:
         player_2_up_pressed = False
-    if key == arcade.key.DOWN:
+    if key == arcade.key.DOWN and not is_ai:
         player_2_down_pressed = False
 
 
@@ -216,6 +260,8 @@ def collision():
     global player_2_x, player_2_y
 
     global ball_x, ball_y, ball_bounce_up, ball_bounce_down, ball_type, ball_velocity, ball_velocity_check
+
+    global is_ai, ai_x, ai_y
 
     # Ball Collision With Divider
     if ball_y + 1 > 490:
@@ -237,7 +283,7 @@ def collision():
         ball_velocity_check = True
         ball_velocity += 1
 
-    if (player_1_x - 10 <= ball_x - 1 < player_1_x + 10) and (player_1_y <= ball_y <= player_1_y + 50):
+    if (player_1_x - 10 <= ball_x - 1 < player_1_x + 10) and (player_1_y <= ball_y <= player_1_y + 50) :
         ball_type = True
         ball_bounce_down = False
         ball_bounce_up = True
@@ -245,19 +291,40 @@ def collision():
         ball_velocity += 1
 
     # Ball Collision With Player 2
-    if (player_2_x - 10 <= ball_x + 1 <= player_2_x + 10) and (player_2_y - 50 <= ball_y <= player_2_y):
+    if (player_2_x - 10 <= ball_x + 1 <= player_2_x + 10) and (player_2_y - 50 <= ball_y <= player_2_y) and not is_ai:
         ball_type = False
         ball_bounce_up = False
         ball_bounce_down = True
         ball_velocity_check = True
         ball_velocity += 1
 
-    if (player_2_x - 10 <= ball_x + 1 <= player_2_x + 10) and (player_2_y <= ball_y <= player_2_y + 50):
+    if (player_2_x - 10 <= ball_x + 1 <= player_2_x + 10) and (player_2_y <= ball_y <= player_2_y + 50) and not is_ai:
         ball_type = False
         ball_bounce_up = True
         ball_bounce_down = False
         ball_velocity_check = True
         ball_velocity += 1
+
+    # Ball Collision With AI
+    if (ai_x - 10 <= ball_x + 1 <= ai_x + 10) and (ai_y - 50 <= ball_y <= ai_y) and is_ai:
+        ball_type = False
+        ball_bounce_up = False
+        ball_bounce_down = True
+        ball_velocity_check = True
+        ball_velocity += 1
+
+    if (ai_x - 10 <= ball_x + 1 <= ai_x + 10) and (ai_y <= ball_y <= ai_y + 50) and is_ai:
+        ball_type = False
+        ball_bounce_up = True
+        ball_bounce_down = False
+        ball_velocity_check = True
+        ball_velocity += 1
+
+
+def start_screen():
+
+    arcade.draw_rectangle_outline(500, 300, 1000, 600, arcade.color.RED)
+
 
 
 def setup():
