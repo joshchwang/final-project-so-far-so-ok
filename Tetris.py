fc949b590a72366e38aeca65965168d6c132c426
josh <joshua.chuang21@ycdsbk12.ca>
2019-07-02 23:34:29 -0400

    done 'i' and 't' blocks for inversion

--- Tetris.py
+++ Tetris.py
@@ -9,9 +9,40 @@ grid = []
 screen_width = (width + margin) * column_count + margin + 200
 screen_height = (height + margin) * row_count + margin
 
+# inversions controls what inversion the block is,
+# if the inversion is:
+# 0 - Upright
+# 1 - On its right side
+# 2 - Upside down
+# 3 - On its left side
+inversions = 0
+
+pressed_right = False
+pressed_left = False
+pressed_down = False
+
 
 def on_update(delta_time):
-    pass
+    global pressed_down, pressed_right, pressed_left
+
+    global inversions
+
+    if pressed_right:
+        if inversions == 3:
+            inversions = 0
+        else:
+            inversions += 1
+
+        pressed_right = False
+        print(inversions)
+
+    if pressed_left:
+        if inversions == 0:
+            inversions = 3
+        else:
+            inversions -= 1
+        pressed_left = False
+        print(inversions)
 
 
 def on_draw():
@@ -45,24 +76,66 @@ def on_draw():
     arcade.draw_rectangle_outline(screen_width - 100, screen_height - 120, 90, 90, arcade.color.WHITE)
     arcade.draw_rectangle_outline(screen_width - 100, screen_height - 120, 75, 75, arcade.color.WHITE)
 
+    blocks(5, 5, inversions, 2)
+
 
 def on_key_press(key, modifiers):
-    pass
+    global pressed_down, pressed_right, pressed_left
+
+    if key == arcade.key.A or key == arcade.key.LEFT:
+        pressed_left = True
+
+    if key == arcade.key.D or key == arcade.key.RIGHT:
+        pressed_right = True
+
+    if key == arcade.key.S or key == arcade.key.DOWN:
+        pressed_down = True
 
 
 def on_key_release(key, modifiers):
-    pass
+    global pressed_down, pressed_right, pressed_left
+
+    if key == arcade.key.A or key == arcade.key.LEFT:
+        pressed_left = False
+
+    if key == arcade.key.D or key == arcade.key.RIGHT:
+        pressed_right = False
+
+    if key == arcade.key.S or key == arcade.key.DOWN:
+        pressed_down = False
 
 
 def on_mouse_press(x, y, button, modifiers):
     pass
 
 
-def block_i(row, column):
-    grid[row][column] = 2
-    grid[row + 1][column] = 2
-    grid[row + 2][column] = 2
-    grid[row + 3][column] = 2
+def block_i(row, column, invert):
+
+    # Upright or inverted
+    if invert == 0 or invert == 2:
+        # Drawing the other inversion state into a neutral cell
+        grid[row][column - 2] = 0
+        grid[row][column - 1] = 0
+        grid[row][column + 1] = 0
+
+        # Drawing the inversion state
+        grid[row - 1][column] = 2
+        grid[row][column] = 2
+        grid[row + 1][column] = 2
+        grid[row + 2][column] = 2
+
+    # Left or right
+    if invert == 1 or invert == 3:
+        # Drawing the other inversion state into a neutral cell
+        grid[row - 1][column] = 0
+        grid[row + 1][column] = 0
+        grid[row + 2][column] = 0
+
+        # Drawing the inversion state
+        grid[row][column - 2] = 2
+        grid[row][column - 1] = 2
+        grid[row][column] = 2
+        grid[row][column + 1] = 2
 
 
 def block_o(row, column):
@@ -72,45 +145,86 @@ def block_o(row, column):
     grid[row - 1][column + 1] = 3
 
 
-def block_t(row, column):
-    grid[row][column] = 4
-    grid[row][column - 1] = 4
-    grid[row][column + 1] = 4
-    grid[row + 1][column] = 4
+def block_t(row, column, invert):
+
+    # Upright
+    if invert == 0:
+        # Drawing the other inversion state into a neutral cell
+        grid[row - 1][column] = 0
+
+        # Drawing the inversion state
+        grid[row][column - 1] = 4
+        grid[row][column] = 4
+        grid[row][column + 1] = 4
+        grid[row + 1][column] = 4
+
+    # On its right side
+    if invert == 1:
+        # Drawing the other inversion state into a neutral cell
+        grid[row][column - 1] = 0
+
+        # Drawing the inversion state
+        grid[row + 1][column] = 4
+        grid[row][column] = 4
+        grid[row - 1][column] = 4
+        grid[row][column + 1] = 4
+
+    # Inverted
+    if invert == 2:
+        # Drawing the other inversion state into a neutral cell
+        grid[row + 1][column] = 0
+
+        # Drawing the inversion state
+        grid[row][column - 1] = 4
+        grid[row][column] = 4
+        grid[row][column + 1] = 4
+        grid[row - 1][column] = 4
+
+    # On its left side
+    if invert == 3:
+        # Drawing the other inversion state into a neutral cell
+        grid[row][column + 1] = 0
+
+        # Drawing the inversion state
+        grid[row + 1][column] = 4
+        grid[row][column] = 4
+        grid[row - 1][column] = 4
+        grid[row][column - 1] = 4
 
 
-def block_s(row, column):
+
+def block_s(row, column, invert):
     grid[row][column] = 5
     grid[row + 1][column] = 5
     grid[row][column - 1] = 5
     grid[row + 1][column + 1] = 5
 
 
-def block_z(row, column):
+def block_z(row, column, invert):
     grid[row][column] = 6
     grid[row + 1][column] = 6
     grid[row][column + 1] = 6
     grid[row + 1][column - 1] = 6
 
 
-def block_j(row, column):
+def block_j(row, column, invert):
     grid[row][column] = 7
     grid[row + 1][column] = 7
     grid[row + 2][column] = 7
     grid[row][column - 1] = 7
 
 
-def block_l(row, column):
+def block_l(row, column, invert):
     grid[row][column] = 8
     grid[row + 1][column] = 8
     grid[row + 2][column] = 8
     grid[row][column + 1] = 8
 
 
-def blocks(row, column, block):
+def blocks(row, column, invert, block):
     # 'I' Block
     if block == 0:
-        block_i(row, column)
+        block_i(row, column, invert)
 
     # 'O' Block
     if block == 1:
@@ -118,23 +232,23 @@ def blocks(row, column, block):
 
     # 'T' Block
     if block == 2:
-        block_t(row, column)
+        block_t(row, column, invert)
 
     # 'S' Block
     if block == 3:
-        block_s(row, column)
+        block_s(row, column, invert)
 
     # 'Z' Block
     if block == 4:
-        block_z(row, column)
+        block_z(row, column, invert)
 
     # 'J' Block
     if block == 5:
-        block_j(row, column)
+        block_j(row, column, invert)
 
     # 'L' Block
     if block == 6:
-        block_l(row, column)
+        block_l(row, column, invert)
 
 
 def setup():
